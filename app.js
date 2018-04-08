04ef4ee5fc38ee0c0cb17487951b0ae0d03a30ec
Tim <tim@timwheeler.com>
2018-04-08 15:23:40 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express'),
+    app = express(),
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server);
+
+server.listen(7777);
+
+app.get('/', function(req, res){
+    res.sendfile(__dirname + '/index.html')
+});
+
+io.sockets.on('connection', function(socket){
+    socket.on('send message', function(data){
+        io.sockets.emit('new message', data);
+    });
+});
